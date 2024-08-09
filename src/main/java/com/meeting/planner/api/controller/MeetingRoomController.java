dd5a6a51eedf1f5b751fc2ab2bcaa94b6efa7c27
hkarkoub <hkarkoub@wynsys.com>
2024-08-09 17:15:13 +0200

    Third commit

--- src/main/java/com/meeting/planner/api/controller/MeetingRoomController.java
+++ src/main/java/com/meeting/planner/api/controller/MeetingRoomController.java
@@ -8,7 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+/** 
+ * 
+ * @author hkarkoub
+ *
+ *
+ */
 @RestController
 @RequestMapping("/api/meetingrooms")
 public class MeetingRoomController {
--- src/main/java/com/meeting/planner/api/service/MeetingRoomService.java
+++ src/main/java/com/meeting/planner/api/service/MeetingRoomService.java
@@ -10,7 +10,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
+/**
+ * 
+ * @author hkarkoub
+ *
+ */
 @Service
 public class MeetingRoomService {
 
